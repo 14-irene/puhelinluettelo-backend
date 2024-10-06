5352d7b9c151c3002e08f939d79f6fb8b419dcd1
ira <mail@ira.pink>
2024-10-06 16:09:07 +0300

    3.1-3.6

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "404-123456"
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-235345"
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (req, res) => res.json(persons))
+app.get('/api/persons/:id', (req, res) => {
+  const person = persons.find(n => n.id === req.params.id)
+  if (person) res.json(person)
+  else res.status(404).send('<h1>404</h1>')
+})
+
+app.get('/info', (request, response) => {
+  response.send(`
+    <div>
+      <p>Phonebook has info for ${persons.length} people<br/>
+      ${new Date(Date.now()).toString()}</p>
+    </div>`
+  )
+})
+
+app.post('/api/persons', (req, res) => {
+  const newId = () => `${Math.floor(Math.random() * Math.pow(10, 16))}`
+  const newPerson = {id: newId(), ...req.body}
+  console.log(newPerson)
+  console.log(persons)
+  if (newPerson) {
+    if (persons.map(p => p.name).includes(newPerson.name)) {
+      return res.status(400).json({error: 'name must be unique'})
+    }
+    else if (newPerson.name && newPerson.number) {
+      persons = persons.concat(newPerson) 
+      res.json(newPerson)
+    }
+    else res.status(400).json({error: 'bad data'})
+  }
+  else res.status(400).json({error: 'no data'})
+
+})
+  
+
+app.delete('/api/persons/:id', (req, res) => {
+  persons = persons.filter(n => n.id !== req.params.id)
+  res.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => console.log(`palvelin portissa ${PORT}`))
